410193591f6ebbd2a2f099533025b5dd075eccff
Charles Wire <charlesk@cointobanks.com>
2019-12-23 16:52:44 +0700

    update

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -16,7 +16,7 @@ def test_lifetioncoind():
     config_text = LifetioncoinConfig.slurp_config_file(config.lifetioncoin_conf)
     network = 'mainnet'
     is_testnet = False
-    genesis_hash = u'00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6'
+    genesis_hash = u'0000032e8b86e9e24ba26fbf815aa656bc0aea20227a45c5d1d36f5dbc532ed3'
     for line in config_text.split("\n"):
         if line.startswith('testnet=1'):
             network = 'testnet'
